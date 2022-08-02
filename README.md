1f87e2359e7ebf562cf540d7b5e79d786375e1d5
Paul Klauser <paul.klauser@gmail.com>
2022-08-01 21:58:48 -0400

    Add README, CHANGELOG
    Use setGrouping() in Sample app

---
+++ README.md
@@ -0,0 +1,23 @@
+# Grouped EditText
+
+An EditText that handles formatting by groups with a custom separator.
+
+## Examples
+`[4, 2, 1], '-'`
+
+![Screenshot 2](screenshots/screen2.png?raw=true)
+
+`[4, 4, 4, 4], ' '`
+
+![Screenshot 1](screenshots/screen1.png?raw=true)
+
+## Supports:
+* Copy/paste
+* Addition/deletion in center of entry
+
+## Gradle Dependency
+
+Make sure you have `mavenCentral()` in your repositories block
+```groovy
+implementation 'io.github.paulklauser:groupededittext:0.1'
+```
--- app/src/main/java/com/paulklauser/groupededittext/sample/MainActivity.kt
+++ app/src/main/java/com/paulklauser/groupededittext/sample/MainActivity.kt
@@ -2,11 +2,14 @@ package com.paulklauser.groupededittext.sample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.paulklauser.groupededittext.GroupedEditText
 import com.paulklauser.groupededittext.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val editText = findViewById<GroupedEditText>(R.id.editText)
+        editText.setGrouping(arrayOf(4, 2, 1), '-')
     }
 }
